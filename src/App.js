b4ef08d67db5869b0fb208004892999531d227f8
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-02-11 11:42:50 +0100

    make the infrastructure

--- src/App.js
+++ src/App.js
@@ -4,9 +4,50 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> hello</h1>
+      <Header />
+      <Form />
+      <ParkingList />
+      <Stats />
     </div>
   );
 }
 
+function Header() {
+  return (
+    <div>
+      <h1>💼far away🌴</h1>
+    </div>
+  );
+}
+
+function Form() {
+  return (
+    <form>
+      <h3>WHat do you want to pack for this trip</h3>
+      <select>
+        <option>1</option>
+      </select>
+      <input />
+      <button>add</button>
+    </form>
+  );
+}
+
+function ParkingList() {
+  return (
+    <div>
+      <ul>
+        <Items />
+      </ul>
+    </div>
+  );
+}
+
+function Items() {
+  return <li></li>;
+}
+
+function Stats() {
+  return <p>You have picked X% of this items</p>;
+}
 export default App;
